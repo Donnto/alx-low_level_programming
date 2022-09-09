ab8315248e375acf96b9a7d15a36267988b1018a
Donnto <donaldmwanga33@gmail.com>
2022-09-09 20:56:49 +0000

    Commit adding whitespaces

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 /*
- *
- * main-displays lowercase alphabet then uppercase alphabet
- * 
- * Return: Always 0 (Success)
- *
- */
+*
+* main-displays lowercase alphabet then uppercase alphabet
+* 
+* Return: Always 0 (Success)
+*
+*/
 int main(void)
 {
 	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 
-	for(i=0; i<52; i++)
+	for (i = 0; i < 52; i++)
 	{
 		putchar(alp[i]);
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
