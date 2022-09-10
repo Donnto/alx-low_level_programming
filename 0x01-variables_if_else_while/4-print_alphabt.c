6660db6080832dad9fcc4c6b9a1c75e0c8e4f43b
Donnto <donaldmwanga33@gmail.com>
2022-09-10 00:27:03 +0000

    commit betty style

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
-/*
- *
- * main-displays lowercase alphabets except q and e
- *
- * Return : Always 0 (Success)
- */
+/**
+* main-displays lowercase alphabets except q and e
+*
+* Return : Always 0 (Success)
+*/
 int main(void)
 {
-	char alp[24] ="abcdfghijklmnoprstuvwxyz";
+	char alp[24] = "abcdfghijklmnoprstuvwxyz";
 	int i;
 
-	for(i=0 ; i<24 ; i++)
+	for (i = 0 ; i < 24 ; i++)
 	{
 		putchar(alp[i]);
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
