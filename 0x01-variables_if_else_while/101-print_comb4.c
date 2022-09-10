43a0f01224abeba17ce8ca817bea14106bcbe856
Donnto <donaldmwanga33@gmail.com>
2022-09-10 04:10:26 +0000

    commit printing the digits combination

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+*
+* main - print all combinations of three different digits
+* Return: Always 0 (Success) 
+*
+*/
+int main(void)
+{
+	int ones, tens, hundreds;
+
+	for (hundreds = '0'; hundreds <= '9'; hundreds++)
+	{
+		for (tens = (hundreds + 1); tens <= '8'; tens++)
+		{
+			for (ones = (tens + 1); ones <= '7'; ones++)
+			{
+				putchar(hundreds);
+				putchar(tens);
+				putchar(ones);
+				if (hundreds != '7' || tens != '8' || ones != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}	
