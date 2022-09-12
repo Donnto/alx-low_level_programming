0618c0fa8a395cd5d428aaca00c4560b0c82a785
Donnto <donaldmwanga33@gmail.com>
2022-09-12 05:50:30 +0000

    commit removing '*' line 3.

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
- *
  * main -Prints all possible combination of two two-digit
  * numbers, ranging from 0 - 99 separated by a comma
  * followed by a space.
  *
  * Return: Always 0 (Success)
- *
  */
 int main(void)
 {
