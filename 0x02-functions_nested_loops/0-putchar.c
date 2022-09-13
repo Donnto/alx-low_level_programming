ee71cf135287bcb7732655a50605c648ebf42c3c
Donnto <donaldmwanga33@gmail.com>
2022-09-13 23:39:04 +0000

    commit printing _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - prints the word _putchar
+ *
+ * Returns: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	char name[12] = "_putchar";
+	int i;
+
+	for( i=0; i<=12; i++)
+	{
+		putchar(name[i]);
+
+	}
+	putchar('\n');
+	return (0);
+}
