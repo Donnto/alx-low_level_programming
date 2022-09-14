2aefdf84beb527e44615f047e80feae4ee8f4a50
Donnto <donaldmwanga33@gmail.com>
2022-09-14 05:15:30 +0000

    commit output string length

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - prints the word _putchar
+ * main - prints the string '_putchar' followed by a
+ * newline
  *
  * Returns: Always 0 (Success)
  *
  */
 int main(void)
 {
-	char name[8] = "_putchar";
+	char name[ ] = "_putchar";
 	int i;
 
 	for (i = 0; i <= 8; i++)
