73146d540c4ba596ac7814f86054a92e8ab44462
Donnto <donaldmwanga33@gmail.com>
2022-09-14 21:08:47 +0000

    commit multiple table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,7 +9,7 @@ void times_table(void)
 	for (num = 0; num <= 9; ++num)
 	{
 		_putchar(48);
-		for (mult = 1; mult <= 9; ++ mult)
+		for (mult = 1; mult <= 9; ++mult)
 		{
 			_putchar(',');
 			_putchar(' ');
@@ -21,16 +21,12 @@ void times_table(void)
 			 * place the first digit if it
 			 * is two numbers
 			 */
-			if (num <= 9)
-			{
+			if (prod <= 9)
 				_putchar(' ');
-			}
 			else
-			{
 				_putchar((prod / 10) + 48);/* get the first digit */
 				_putchar((prod % 10) + 48);/* get the second digit */
-			}
-			_putchar('\n');
 		}
+		_putchar('\n');
 	}
 }
