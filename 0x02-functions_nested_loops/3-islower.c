db8aa8daa2fe3bb843820658d3857386507c18b4
Donnto <donaldmwanga33@gmail.com>
2022-09-14 11:35:10 +0000

    commit removing whitespace

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,20 +2,20 @@
 /**
  * _islower- checks for lowercase character
  *
- * @c is the int we will use for the argument of the
+ * @c: is the int we will use for the argument of the
  * fuction.
  *
- * Return : 1 if c is lower return 0 otherwise
+ * Return : 0
  */
-int _islower (int c)
+int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
 	{
-		return(1);
+		return (1);
 	}
 	else
 	{
-		return(0);
+		return (0);
 	}
 	_putchar('\n');
 }
