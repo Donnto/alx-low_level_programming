0cd634e72a77a90f93d2391aaddcd1365d893ede
Donnto <donaldmwanga33@gmail.com>
2022-09-14 21:43:28 +0000

    commit addition

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * add - adds two interger numbers and return the results
- * @a - first number
- * @b - second number
+ * @a: first number
+ * @b: second number
  * Return: sum of a and b.
  */
 int add(int a,  int b)
