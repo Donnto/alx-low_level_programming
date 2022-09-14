2db28457b4c85a9a614613decbba882673efd43e
Donnto <donaldmwanga33@gmail.com>
2022-09-14 19:00:42 +0000

    commit jack baure

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,14 +9,14 @@ void jack_baure(void)
 	int hour = 0;
 	int minute = 0;
 	int hour_remainder;
-	int minutes_remainder;
+	int minute_remainder;
 
 	while (hour <= 23)
 	{
 	while (minute <= 59)
 	{
 
-	hours_remainder = hour % 10;
+	hour_remainder = hour % 10;
 	_putchar(hour /10 + '0');
 	_putchar(hour_remainder + '0');
 	_putchar(':');
