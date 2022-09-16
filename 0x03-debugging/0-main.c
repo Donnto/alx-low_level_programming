89062ee518c0995a195da7c5df398105d21d525b
Donnto <donaldmwanga33@gmail.com>
2022-09-16 15:04:44 +0000

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
  * main - test function that prints if interger is
  * positive or negative 
  * Return : 0
  */
+
 int main(void)
 {
 int i;
@@ -11,6 +13,5 @@ int i;
 i = 0;
 positive_or_negative(i);
 
-putchar("\n");
-return(0);
+return (0);
 }
