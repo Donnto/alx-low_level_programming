9455686e7c17e0ef7e35da311fec772ef2ccc252
Donnto <donaldmwanga33@gmail.com>
2022-09-16 12:37:18 +0000

    largest interger

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * Largest_number -return the largest of 3 numbers 
+ * @a:first interger
+ * @b:second interger
+ * @c:third interger
+ * Return: Largest number
+ */
+
+int largest_number (int a, int b, int c)
+{
+	int interger;
+	if (a >= b && a >= c)
+	{
+		largest = a;
+		{
+			else if (b > a && b >= c)
+			{
+				largest = b;
+			}
+			else
+			{
+				largest = c;
+			}
+			return (Largest);
+		}
+	}
+}
