b2034442aa75d94ce0859c5e24ff829f1e44098f
Donnto <donaldmwanga33@gmail.com>
2022-09-16 12:58:31 +0000

    largest_number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,20 +10,18 @@
 
 int largest_number (int a, int b, int c)
 {
-	int interger;
-	if (a >= b && a >= c)
-	{
-		largest = a;
-		{
-			else if (b > a && b >= c)
-			{
-				largest = b;
-			}
-			else
-			{
-				largest = c;
-			}
-			return (Largest);
-		}
-	}
+int interger;
+if (a >= b && a >= c)
+{
+largest = a;
+}
+else if (b > a && b >= c)
+{
+largest = b;
+}
+else
+{
+largest = c;
+}
+return (Largest);
 }
