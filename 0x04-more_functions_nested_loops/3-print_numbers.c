4259a0203bcc6100376dabfe006c75fe485e2dc1
Donnto <donaldmwanga33@gmail.com>
2022-09-17 03:40:14 +0000

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,14 @@
-#include "main"
+#include "main.h"
 
 /**
- * print_numbers - prints numbers between 0 to 9.
- * Return: no return.
+ * print_numbers - prints numbers between 0 to 9
+ * Return: void
  */
 void print_numbers(void)
 {
 	int ch;
 
-	for (ch = 48; ch < 58; ch++)
+	for (ch = 48; ch <= 57; ch++)
 	{
 		_putchar(ch);
 	}
