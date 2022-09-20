253110e7cb1895017b39e930d90bd6d823cf008b
Donnto <donaldmwanga33@gmail.com>
2022-09-20 19:40:47 +0000

    print array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,22 +11,16 @@
  *
  * Returns : no return value.
  */
-void print_array(int *a, int n);
+
+void print_array(int *a, int n)
 {
 	int i = 0;
 
-	while (i < n)
+	for (; i < n; i++)
 	{
-		if (i < (n - 1))
-		{
-			printf("%i, ", a[i]);
-		}
-		else
-		{
-			printf("%i", a[i]);
-		}
-		i++;
+		printf("%d", *(a + i));
+		if (i != (n - 1))
+			printf(", ");
 	}
 	printf("\n");
 }
-
