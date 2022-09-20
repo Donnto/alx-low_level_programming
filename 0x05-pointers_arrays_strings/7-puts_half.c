23b39bbf099861b471d72d0f4add80417a24bd4e
Donnto <donaldmwanga33@gmail.com>
2022-09-20 18:59:07 +0000

    commit puts half string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - function that prints half a string followed
+ * by a new line 
+ *
+ * @str: a string
+ *
+ * Return: no return value
+ */
+
+void puts_half(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	i = (i + 1) / 2;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
