494ff29653e3e8ff424ccb56914cadd84e69ca25
Donnto <donaldmwanga33@gmail.com>
2022-09-22 10:17:51 +0000

    commit return value

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -6,9 +6,12 @@
  * of bytes from string src into dest.
  * @dest: The buffer storing the string copy.
  * @src: The source string.
- * @i: index.
  * @n: The maximum number of bytes to copied from src.
+ * @i: index
+ *
+ * Return: A pointer to the resulting string dest.
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0, src_len = 0;
