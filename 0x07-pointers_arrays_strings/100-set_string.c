08989641e8b6ee6cc10a7a43e5842eb71e315dbd
Donnto <donaldmwanga33@gmail.com>
2022-09-26 23:35:38 +0000

    commit set_string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "maun.h"
+
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: Source address.
+ * @to: target address.
+ * Return: no return:
+ */
+void set_string(char **s, char *to)
+{
+	*s = to
+}
