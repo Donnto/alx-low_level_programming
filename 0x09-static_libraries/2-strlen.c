0fb031c8b30c90a6d9840a5e43a82fd0e6c38ead
Donald Mwanga <donaldmwanga33@gmail.com>
2022-10-02 13:26:02 +0000

    commit static libraries

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - starting point of a function.
+ *
+ * @s: a string
+ *
+ * Return: length of string as interger.
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*(s + i))
+		i++;
+	return (i);
+}
