90856ebcb32b024001071ace96e29bae240b02e6
Donald Mwanga <donaldmwanga33@gmail.com>
2022-10-05 09:44:50 +0000

    static_libraries

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,12 +1,7 @@
 #include "main.h"
 
-/**
- * main - function that puts a string to stdout.
- *
- * Return: no return.
- */
 int main(void)
 {
-	_puts("\"At the end of the day, My goal was to be the best hacker\"\n\t- Kevin Mitnick");
-	return (0);
+    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+    return (0);
 }
