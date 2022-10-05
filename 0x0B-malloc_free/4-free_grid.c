59bf15c25fef1405674697da7154c821be773068
Donald Mwanga <donaldmwanga33@gmail.com>
2022-10-05 06:48:00 +0000

    free grid

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * free_grid - Frees a 2- dimensional array of intergers.
@@ -19,4 +19,3 @@ void free_grid(int **grid, int height)
 		free(grid);
 	}
 }
-
