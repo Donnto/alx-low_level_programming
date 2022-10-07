94d0ed73904d5d11fe6d78dff62d048a1e2f9e81
Donald Mwanga <donaldmwanga33@gmail.com>
2022-10-07 01:23:48 +0000

    commit return value

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -5,7 +5,9 @@
  * malloc_checked - Allocates memory using malloc.
  * @b: The number of bytes to be allocated.
  *
+ * Return: Return: A pointer to the allocated memory.
  */
+
 void *malloc_checked(unsigned int b)
 {
 	void *mem = malloc(b);
