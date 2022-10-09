285706009478109a13ad5ba072c68770c37f12b4
Donald Mwanga <donaldmwanga33@gmail.com>
2022-10-09 01:27:46 +0000

    multipling two numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -4,32 +4,32 @@
 /**
  * main - multiplies two numbers as arguments
  * @argc:number of arguments.
- * @agrv:Arguments.
+ * @argv:arguments.
  *
  * Return:0 success.
  */
-int main(int argc,char*argv[])
+int main(int argc, char *argv[])
 {
 	unsigned long mul;
-	int i,j;
+	int i, j;
 
-	if(argc != 3)
+	if (argc != 3)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	for(i = 1; i<argc; i++)
+	for (i = 1; i < argc; i++)
 	{
-		for(j=0; argv[i][j] != '\0'; j++)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if(argv[i][j] > 57 || argv[i][j] < 48)
+			if (argv[i][j] > 57 || argv[i][j] < 48)
 			{
 				printf("Error\n");
 				exit(98);
 			}
 		}
 		mul = atol(argv[1]) * atol(argv[2]);
-		printf("%lu\n",mul);
+		printf("%lu\n", mul);
 	}
 	return (0);
 }
